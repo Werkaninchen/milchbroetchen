b6c2fdda1a5dca69db605fcde1248ad7a48bdda7
ronnyboy@samsung <dekara@gmx.de>
2019-05-04 19:11:24 +0200

    Screen edited

--- godot/GameView/Screen.gd
+++ godot/GameView/Screen.gd
@@ -1,13 +1,29 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var character_scene = preload("res://character/Character.tscn")
+
+var player_screen = preload("res://GameView/PlayerScreen.tscn")
+
+var players
+
+var screens = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	for player in players:
+		var new_player_screen = player_screen.instance()
+		if screens.size() < 2:
+			$HBC/VBC.add_child(new_player_screen)
+		else:
+			$HBC/VBC2.add_child(new_player_screen)
+		
+		screens.append(new_player_screen)
+		players[player].char_scene = new_player_screen
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func setup_players(players):
+	self.players = players
+	for player in self.players:
+		var new_player = character_scene.instance()
+		self.players[player].char_scene = new_player
+		$World.add_child(new_player)
+	
