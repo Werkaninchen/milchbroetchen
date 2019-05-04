295496159b9da2f421896e7ccecf0fecf69e534a
Neolyum <jojojonasjojo@gmail.com>
2019-05-04 19:54:40 +0200

    test button created :D

---
+++ godot/Level/AudioStreamPlayer2.gd
@@ -0,0 +1,22 @@
+extends AudioStreamPlayer
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("test2")
+	play()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Button_pressed():
+	play()
+	print("test")
+
+func _on_TouchScreenButton_released():
+	play()
