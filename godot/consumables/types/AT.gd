1bbfdd5edcece4abe19e2507ca75723ec37ec9ad
Antonia Holsten <antonia.holsten@hotmail.de>
2019-05-04 19:59:54 +0200

    added attack and health consumable

--- godot/consumables/types/AT.gd
+++ godot/consumables/types/AT.gd
@@ -2,19 +2,22 @@ extends "res://consumables/base/Consumable.gd"
 var character = load("res://character/Character.gd")
 
 var linkBody = null
-export var m_speed = 2
+export var m_attack = 2
 
 func init_type():
-	consumableType = enums.ConsumType.Speed
+	consumableType = enums.ConsumType.Attack
 
 func on_body_entered(body):
+	print("body entered")
 	if body is character:
+		print("is character")
 		linkBody = body
-		linkBody.max_speed = linkBody.max_speed * m_speed
+		linkBody.attack_power = linkBody.attack_power * m_attack
 		get_child(0).hide()
 		timer.start(duration)
 	pass
 	
 func on_timeout():
-	linkBody.max_speed = linkBody.max_speed / m_speed
+	print("on timeout")
+	linkBody.attack_power = linkBody.attack_power / m_attack
 	queue_free()
--- godot/global/enums.gd
+++ godot/global/enums.gd
@@ -3,8 +3,9 @@ extends Node
 enum ConsumType{
 	XP,
 	Speed,
-	Size
-	}
+	Attack,
+	Health
+}
 	
 func get_rand_consumType():
 	return enums.ConsumType[rand_range(0, enums.ConsumType.size())]
